52d93581eb2efa221789dd8df02b135443e30a64
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-08-31 16:17:46 -0500

    update test added

--- src/routes.ts
+++ src/routes.ts
@@ -12,11 +12,36 @@ router.post("/notes", (req: Request, res: Response) => {
   const newNote = {
     id: notes.length + 1,
     title,
-    content
-  }
+    content,
+  };
 
-  notes.push(newNote)
-  res.status(201).json(newNote)
+  notes.push(newNote);
+  res.status(201).json(newNote);
+});
+
+router.get("/notes/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    res.status(200).json(note);
+  } else {
+    res.status(404).json({ message: "Note not Found" });
+  }
+});
+
+router.put("/notes/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  const note = notes.find((note) => note.id === id);
+  const { content, title } = req.body;
+
+  if (note) {
+    note.content = content;
+    note.title = title;
+    res.status(202).json(note);
+  } else {
+    res.status(404).json({ message: "Note not Found" });
+  }
 });
 
 export default router;
--- tests/notes.test.ts
+++ tests/notes.test.ts
@@ -11,8 +11,42 @@ describe("Notes CRUD API", () => {
     const newNote = { title: "Test Title", content: "Test content" };
     const response = await supertest(app).post("/notes").send(newNote);
 
-    expect(response.statusCode).toBe(201)
-    expect(response.body.title).toBe(newNote.title)
-    expect(response.body.content).toBe(newNote.content)
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe(newNote.title);
+    expect(response.body.content).toBe(newNote.content);
+  });
+  it("should read a note", async () => {
+    const newNote = {
+      title: "Test Title for read",
+      content: "Test content for read",
+    };
+    const creationResponse = await supertest(app).post("/notes").send(newNote);
+    const response = await supertest(app).get(
+      `/notes/${creationResponse.body.id}`
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body.title).toBe(newNote.title);
+    expect(response.body.content).toBe(newNote.content);
+  });
+
+  it("should update an existing note", async () => {
+    const newNote = {
+      title: "Test Title for update",
+      content: "Test content for update",
+    };
+    const creationResponse = await supertest(app).post("/notes").send(newNote);
+
+    const updatedNote = {
+      title: "Updated Title",
+      content: "Updated Content",
+    };
+    const response = await supertest(app)
+      .put(`/notes/${creationResponse.body.id}`)
+      .send(updatedNote);
+
+    expect(response.status).toBe(202);
+    expect(response.body.title).toBe(updatedNote.title);
+    expect(response.body.content).toBe(updatedNote.content);
   });
 });
